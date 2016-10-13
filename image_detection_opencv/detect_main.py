7851297cef64bdf96f6443c49d7c31278501a356
Jia Li <bigben80@gmail.com>
2016-10-13 20:31:44 +0000

    - remove config.json

--- image_detection_opencv/detect_main.py
+++ image_detection_opencv/detect_main.py
@@ -13,8 +13,10 @@ from picamera.array import PiRGBArray
 from picamera import PiCamera
 import cv2
 
-from flask import Flask, render_template, Response
-from picamera import PiCamera
+from flask import Flask, render_template, Response, redirect, url_for, request, session, abort
+from flask_login import LoginManager, UserMixin, \
+                                login_required, login_user, logout_user
+
 from myvideostream import MyPiVideoStream
 import time
 
@@ -37,10 +39,37 @@ FROM_SERVICE_NUMBER = "+46769447309"
 haarcascade_path = './'
 
 
-vs = MyPiVideoStream(resolution=(320, 240), rotation=180).start()
+vs = MyPiVideoStream(resolution=(320, 240)).start()
 time.sleep(2)
 
 app = Flask(__name__)
+
+# config
+app.config.update(
+    DEBUG = True,
+    SECRET_KEY = 'secret_xxx'
+)
+
+# flask-login
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+username = json.load(open("config.json"))["username"]
+password = json.load(open("config.json"))["password"]
+
+class User(UserMixin):
+
+    def __init__(self, id):
+        self.id = id
+        self.name = username
+        self.password = password
+
+    def __repr__(self):
+        return "%d/%s/%s" % (self.id, self.name, self.password)
+
+
+users = [User(1)]
 
 def detect_face(gray):
 
@@ -254,9 +283,51 @@ def gen():
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/video_feed')
+@login_required
 def video_feed():
     return Response(gen(),
                 mimetype='multipart/x-mixed-replace; boundary=frame')
+
+# somewhere to login
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == 'POST':
+        usernm = request.form['username']
+        passwd = request.form['password']
+        if passwd == password and usernm == username:
+            id = 1
+            user = User(id)
+            login_user(user)
+            return redirect(request.args.get("next"))
+        else:
+            return abort(401)
+    else:
+        return Response('''
+        <form action="" method="post">
+            <p><input type=text name=username>
+            <p><input type=password name=password>
+            <p><input type=submit value=Login>
+        </form>
+        ''')
+
+# somewhere to logout
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return Response('<p>Logged out</p>')
+
+
+# handle login failed
+@app.errorhandler(401)
+def page_not_found(e):
+    return Response('<p>Login failed</p>')
+
+
+# callback to reload the user object
+@login_manager.user_loader
+def load_user(userid):
+    return User(userid)
 
 
 def main(config_file):
--- image_detection_opencv/server.py
+++ image_detection_opencv/server.py
@@ -1,40 +1,87 @@
-#!/usr/bin/env python
-from flask import Flask, render_template, Response
-from picamera import PiCamera
-from myvideostream import MyPiVideoStream
-import time
-
-from OpenSSL import SSL
-
-from gevent.pywsgi import WSGIServer
-
-vs = MyPiVideoStream(rotation=180).start()
-time.sleep(1)
- 
+from flask import Flask, Response, redirect, url_for, request, session, abort
+from flask_login import LoginManager, UserMixin, \
+                                login_required, login_user, logout_user 
+import json
 
 app = Flask(__name__)
 
+# config
+app.config.update(
+    DEBUG = True,
+    SECRET_KEY = 'secret_xxx'
+)
+
+# flask-login
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+conf = json.load(open("config.json"))
+
+class User(UserMixin):
+
+    def __init__(self, id):
+        self.id = id
+        self.name = conf["username"]
+        self.password = conf["password"]
+        
+    def __repr__(self):
+        return "%d/%s/%s" % (self.id, self.name, self.password)
+
+
+users = [User(1)]
+
+
+# some protected url
 @app.route('/')
-def index():
-    return render_template('index.html')
+@login_required
+def home():
+    return Response("Hello World!")
 
-def gen():
-#    vs = MyPiVideoStream(rotation=180).start()
-#    time.sleep(1)
-    while True:
-        frame = vs.read_consistent()
+ 
+# somewhere to login
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']        
+        if password == conf["password"] and username == conf["username"]:
+            id = 1
+            user = User(id)
+            login_user(user)
+            return redirect(request.args.get("next"))
+        else:
+            return abort(401)
+    else:
+        return Response('''
+        <form action="" method="post">
+            <p><input type=text name=username>
+            <p><input type=password name=password>
+            <p><input type=submit value=Login>
+        </form>
+        ''')
 
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen(),
-                mimetype='multipart/x-mixed-replace; boundary=frame')
+# somewhere to logout
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return Response('<p>Logged out</p>')
 
-if __name__ == '__main__':
 
-    http_server = WSGIServer(('0.0.0.0', 7737), app, keyfile='domain.key', certfile='domain.crt')
-    http_server.serve_forever()
+# handle login failed
+@app.errorhandler(401)
+def page_not_found(e):
+    return Response('<p>Login failed</p>')
+    
+    
+# callback to reload the user object        
+@login_manager.user_loader
+def load_user(userid):
+    return User(userid)
+    
 
+if __name__ == "__main__":
+    app.run('0.0.0.0')
 
